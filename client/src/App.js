5952a16c15aa74c646cafd6275312748c6a70d81
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2025-03-29 19:45:21 +0530

    initial setup of train-seat-booking project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Train Seat Booking</h1>
+    </div>
+  );
+}
+
+export default App;
