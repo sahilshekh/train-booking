40192a8d02a6f8ab753e0f0eb0d7bcef25be6dce
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2025-03-30 09:32:41 +0530

    cors origin error resolved for mobile

--- server/api/index.js
+++ server/api/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // CORS middleware with preflight support
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   if (req.method === 'OPTIONS') {
